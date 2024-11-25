de804e3d23d3f8e40e9773696b277fe9e6e6f0af
AgniKoko <agnikoko@ee.duth.gr>
2024-11-25 22:02:31 +0200

    Successfully combined panorama12 & panorama34 into final_panorama (wrong usage of k12, k34).

--- OpenPano_flower_panorama.py
+++ OpenPano_flower_panorama.py
@@ -11,7 +11,7 @@ def resize_image(img, width=500):
     new_height = int(width * aspect_ratio)
     return cv2.resize(img, (width, new_height))
 
-img1 = cv.imread('img/GES-50/0.jpg')
+img1 = cv.imread('img/OpenPano/flower/1.jpg')
 img1 = resize_image(img1)
 cv.namedWindow('main1')
 cv.imshow('main1', img1)
@@ -19,7 +19,7 @@ cv.waitKey(0)
 kp1 = sift.detect(img1)
 desc1 = sift.compute(img1, kp1)
 
-img2 = cv.imread('img/GES-50/1.jpg')
+img2 = cv.imread('img/OpenPano/flower/2.jpg')
 img2 = resize_image(img2)
 cv.namedWindow('main2')
 cv.imshow('main2', img2)
@@ -27,7 +27,7 @@ cv.waitKey(0)
 kp2 = sift.detect(img2)
 desc2 = sift.compute(img2, kp2)
 
-img3 = cv.imread('img/GES-50/2.jpg')
+img3 = cv.imread('img/OpenPano/flower/3.jpg')
 img3 = resize_image(img3)
 cv.namedWindow('main2')
 cv.imshow('main2', img3)
@@ -35,7 +35,7 @@ cv.waitKey(0)
 kp3 = sift.detect(img3)
 desc3 = sift.compute(img3, kp3)
 
-img4 = cv.imread('img/GES-50/3.jpg')
+img4 = cv.imread('img/OpenPano/flower/4.jpg')
 img4 = resize_image(img4)
 cv.namedWindow('main2')
 cv.imshow('main2', img4)
@@ -79,13 +79,13 @@ def match_features(d1, d2, method='match1'):
 
     return matches
 
-matches = match_features(desc1[1], desc2[1], method='match1')
-matches = match_features(desc1[1], desc2[1], method='match2')
+matches12 = match_features(desc1[1], desc2[1], method='match1')
+matches12 = match_features(desc1[1], desc2[1], method='match2')
 
-dimg = cv.drawMatches(img1, desc1[0], img2, desc2[0], matches, None)
+dimg12 = cv.drawMatches(img1, desc1[0], img2, desc2[0], matches12, None)
 cv.namedWindow('main3')
-cv.imshow('main3', dimg)
-cv2.imwrite('results/GES-50/dimg.jpg', dimg)
+cv.imshow('main3', dimg12)
+cv2.imwrite('results/OpenPano/flower/dimg12.jpg', dimg12)
 cv.waitKey(0)
 
 img_pt1 = []
@@ -102,36 +102,41 @@ def create_panorama(img1, img2, kp1, kp2, desc1, desc2, matches):
 
     return panorama
 
-panorama12 = create_panorama(img1, img2, kp1, kp2, desc1, desc2, matches)
+panorama12 = create_panorama(img1, img2, kp1, kp2, desc1, desc2, matches12)
 cv.namedWindow('panorama12')
 cv.imshow('panorama12', panorama12)
-cv2.imwrite('results/GES-50/panorama12.jpg', panorama12)
+cv2.imwrite('results/OpenPano/flower/panorama12.jpg', panorama12)
 kp12 = sift.detect(panorama12)
 desc12 = sift.compute(panorama12, kp12)
 cv.waitKey(0)
 
-matches = match_features(desc3[1], desc4[1], method='match1')
-matches = match_features(desc3[1], desc4[1], method='match2')
-panorama34 = create_panorama(img3, img4, kp3, kp4, desc3, desc4, matches)
+matches34 = match_features(desc3[1], desc4[1], method='match1')
+matches34 = match_features(desc3[1], desc4[1], method='match2')
+panorama34 = create_panorama(img3, img4, kp3, kp4, desc3, desc4, matches34)
 cv.namedWindow('panorama34')
 cv.imshow('panorama34', panorama34)
-cv2.imwrite('results/GES-50/panorama34.jpg', panorama34)
+cv2.imwrite('results/OpenPano/flower/panorama34.jpg', panorama34)
 kp34 = sift.detect(panorama34)
 desc34 = sift.compute(panorama34, kp34)
 cv.waitKey(0)
 
-final_dimg = cv.drawMatches(img1, desc1[0], img2, desc2[0], matches, None)
+dimg34 = cv.drawMatches(img1, desc1[0], img2, desc2[0], matches34, None)
+cv.namedWindow('main3')
+cv.imshow('main3', dimg34)
+cv2.imwrite('results/OpenPano/flower/dimg34.jpg', dimg34)
+
+matches_final = match_features(desc12[1], desc34[1], method='match1')
+matches_final = match_features(desc12[1], desc34[1], method='match2')
+final_dimg = cv.drawMatches(img1, desc1[0], img2, desc2[0], matches_final, None)
 cv.namedWindow('final_dimg')
 cv.imshow('final_dimg', final_dimg)
-cv2.imwrite('results/GES-50/final_dimg.jpg', final_dimg)
+cv2.imwrite('results/OpenPano/flower/final_dimg.jpg', final_dimg)
 cv.waitKey(0)
 
-matches = match_features(desc12[1], desc34[1], method='match1')
-matches = match_features(desc12[1], desc34[1], method='match2')
-final_panorama = create_panorama(panorama12, panorama34, kp1, kp4, desc12, desc34, matches)
+final_panorama = create_panorama(panorama12, panorama34, kp12, kp34, desc12, desc34, matches_final)
 cv.namedWindow('final_panorama')
 cv.imshow('final_panorama', final_panorama)
-cv2.imwrite('results/GES-50/final_panorama.jpg', final_panorama)
+cv2.imwrite('results/OpenPano/flower/final_panorama.jpg', final_panorama)
 cv.waitKey(0)
 
 pass
