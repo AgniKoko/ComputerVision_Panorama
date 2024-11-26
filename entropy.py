bdae6cfe8ab3042c3d0cbd6e71f150e1c431ac24
AgniKoko <agnikoko@ee.duth.gr>
2024-11-26 23:30:39 +0200

    Final calculation of avg local entropy (f3).

--- entropy.py
+++ entropy.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 from skimage.measure import shannon_entropy
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
 
 component_1 = cv2.imread('results/OpenPano/flower/dimg12.jpg', cv2.IMREAD_GRAYSCALE)
 component_2 = cv2.imread('results/OpenPano/flower/dimg34.jpg', cv2.IMREAD_GRAYSCALE)
+component_images = [component_1, component_2]
 final_panorama = cv2.imread('results/OpenPano/flower/final_panorama.jpg', cv2.IMREAD_GRAYSCALE)
 
 def global_entropy(image):
@@ -18,7 +21,15 @@ def calculate_f2(component_images, panorama_image):
     f2 = avg_entropy_components - panorama_entropy
     return f2
 
-component_images = [component_1, component_2]
 f2 = calculate_f2(final_panorama, component_images)
-
 print(f"f2 (Differential Entropy): {f2}")
+
+def local_entropy(image, window_size=9):
+    return entropy(image, disk(window_size // 2))
+
+def calculate_f3(panorama_image):
+    local_entropy_panorama = local_entropy(panorama_image)
+    return np.mean(local_entropy_panorama)
+
+f3 = calculate_f3(final_panorama)
+print(f"f3 (Average Local Entropy): {f3}")
