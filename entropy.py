cdd7ecc18688c79b5fbbdd4600bbcc49226434f1
AgniKoko <agnikoko@ee.duth.gr>
2024-11-26 23:40:12 +0200

    Calculation of differential variance of local entropy (f4).

--- entropy.py
+++ entropy.py
@@ -9,6 +9,8 @@ component_2 = cv2.imread('results/OpenPano/flower/dimg34.jpg', cv2.IMREAD_GRAYSC
 component_images = [component_1, component_2]
 final_panorama = cv2.imread('results/OpenPano/flower/final_panorama.jpg', cv2.IMREAD_GRAYSCALE)
 
+# ==============================  f2  ==============================
+
 def global_entropy(image):
     return shannon_entropy(image)
 
@@ -24,6 +26,8 @@ def calculate_f2(component_images, panorama_image):
 f2 = calculate_f2(final_panorama, component_images)
 print(f"f2 (Differential Entropy): {f2}")
 
+# ==============================  f3  ==============================
+
 def local_entropy(image, window_size=9):
     return entropy(image, disk(window_size // 2))
 
@@ -33,3 +37,18 @@ def calculate_f3(panorama_image):
 
 f3 = calculate_f3(final_panorama)
 print(f"f3 (Average Local Entropy): {f3}")
+
+# ==============================  f4  ==============================
+
+def calculate_f4(component_images, panorama_image, window_size=9):
+    local_entropies_components = [local_entropy(img, window_size) for img in component_images]
+    variance_components = [np.var(ent) for ent in local_entropies_components]
+    avg_variance_components = np.mean(variance_components)
+
+    local_entropy_panorama = local_entropy(panorama_image, window_size)
+    variance_panorama = np.var(local_entropy_panorama)
+
+    return variance_panorama - avg_variance_components
+
+f4 = calculate_f4(component_images, final_panorama)
+print(f"f4 (Differential Variance of Local Entropy): {f4}")
