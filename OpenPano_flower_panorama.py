ba1152839d74c5361c1231860e7369f191ddc94f
AgniKoko <agnikoko@ee.duth.gr>
2024-11-27 11:29:24 +0200

    Updated f2

--- OpenPano_flower_panorama.py
+++ OpenPano_flower_panorama.py
@@ -3,41 +3,23 @@ import numpy as np
 import cv2 as cv
 
 sift = cv.xfeatures2d_SIFT.create(400)
-cv.namedWindow('main1')
+cv.namedWindow('img1')
 
-def resize_image(img, width=500):
-    h, w = img.shape[:2]
-    aspect_ratio = h / w
-    new_height = int(width * aspect_ratio)
-    return cv2.resize(img, (width, new_height))
+# ==============================  12  ==============================
 
 img1 = cv.imread('img/OpenPano/flower/1.jpg')
-cv.namedWindow('main1')
-cv.imshow('main1', img1)
+cv.namedWindow('img1')
+cv.imshow('img1', img1)
 cv.waitKey(0)
 kp1 = sift.detect(img1)
 desc1 = sift.compute(img1, kp1)
 
 img2 = cv.imread('img/OpenPano/flower/2.jpg')
-cv.namedWindow('main2')
-cv.imshow('main2', img2)
+cv.namedWindow('img2')
+cv.imshow('img2', img2)
 cv.waitKey(0)
 kp2 = sift.detect(img2)
 desc2 = sift.compute(img2, kp2)
-
-img3 = cv.imread('img/OpenPano/flower/3.jpg')
-cv.namedWindow('main2')
-cv.imshow('main2', img3)
-cv.waitKey(0)
-kp3 = sift.detect(img3)
-desc3 = sift.compute(img3, kp3)
-
-img4 = cv.imread('img/OpenPano/flower/4.jpg')
-cv.namedWindow('main2')
-cv.imshow('main2', img4)
-cv.waitKey(0)
-kp4 = sift.detect(img4)
-desc4 = sift.compute(img4, kp4)
 
 def match_features(d1, d2, method='match1'):
     n1 = d1.shape[0]
@@ -79,8 +61,8 @@ matches12 = match_features(desc1[1], desc2[1], method='match1')
 matches12 = match_features(desc1[1], desc2[1], method='match2')
 
 dimg12 = cv.drawMatches(img1, desc1[0], img2, desc2[0], matches12, None)
-cv.namedWindow('main3')
-cv.imshow('main3', dimg12)
+cv.namedWindow('dimg12')
+cv.imshow('dimg12', dimg12)
 cv2.imwrite('results/OpenPano/flower/dimg12.jpg', dimg12)
 cv.waitKey(0)
 
@@ -106,6 +88,27 @@ kp12 = sift.detect(panorama12)
 desc12 = sift.compute(panorama12, kp12)
 cv.waitKey(0)
 
+gray12 = cv2.cvtColor(panorama12, cv2.COLOR_BGR2GRAY)
+_, thresh12 = cv2.threshold(gray12, 1, 255, cv2.THRESH_BINARY)
+x, y, w, h = cv2.boundingRect(thresh12)
+cropped_panorama12 = panorama12[y:y+h, x:x+w]
+
+# ==============================  34  ==============================
+
+img3 = cv.imread('img/OpenPano/flower/3.jpg')
+cv.namedWindow('img3')
+cv.imshow('img3', img3)
+cv.waitKey(0)
+kp3 = sift.detect(img3)
+desc3 = sift.compute(img3, kp3)
+
+img4 = cv.imread('img/OpenPano/flower/4.jpg')
+cv.namedWindow('img4')
+cv.imshow('img4', img4)
+cv.waitKey(0)
+kp4 = sift.detect(img4)
+desc4 = sift.compute(img4, kp4)
+
 matches34 = match_features(desc3[1], desc4[1], method='match1')
 matches34 = match_features(desc3[1], desc4[1], method='match2')
 panorama34 = create_panorama(img3, img4, kp3, kp4, desc3, desc4, matches34)
@@ -121,16 +124,12 @@ cv.namedWindow('main3')
 cv.imshow('main3', dimg34)
 cv2.imwrite('results/OpenPano/flower/dimg34.jpg', dimg34)
 
-# Find non-black area
-gray12 = cv2.cvtColor(panorama12, cv2.COLOR_BGR2GRAY)
-_, thresh12 = cv2.threshold(gray12, 1, 255, cv2.THRESH_BINARY)
-x, y, w, h = cv2.boundingRect(thresh12)
-cropped_panorama12 = panorama12[y:y+h, x:x+w]
-
 gray34 = cv2.cvtColor(panorama34, cv2.COLOR_BGR2GRAY)
 _, thresh12 = cv2.threshold(gray34, 1, 255, cv2.THRESH_BINARY)
 x, y, w, h = cv2.boundingRect(thresh12)
 cropped_panorama34 = panorama34[y:y+h, x:x+w]
+
+# ==============================  final  ==============================
 
 matches_final = match_features(desc12[1], desc34[1], method='match1')
 matches_final = match_features(desc12[1], desc34[1], method='match2')
--- entropy.py
+++ entropy.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-from skimage.measure import shannon_entropy
-from skimage.filters.rank import entropy
-from skimage.morphology import disk
+from scipy.stats import entropy
+
 
 component_1 = cv2.imread('results/OpenPano/flower/dimg12.jpg', cv2.IMREAD_GRAYSCALE)
 component_2 = cv2.imread('results/OpenPano/flower/dimg34.jpg', cv2.IMREAD_GRAYSCALE)
@@ -11,15 +10,28 @@ final_panorama = cv2.imread('results/OpenPano/flower/final_panorama.jpg', cv2.IM
 
 # ==============================  f2  ==============================
 
-def global_entropy(image):
-    return shannon_entropy(image)
+def calculate_global_entropy(image):
+    hist, _ = np.histogram(image, bins=256, range=(0, 256), density=True)
+    hist = hist[hist > 0]  # Avoid log(0)
+    return entropy(hist, base=2)
+
+
+def calculate_local_entropy(image, window_size=9):
+    padded_image = np.pad(image, pad_width=window_size // 2, mode='reflect')
+    local_entropies = []
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            # Extract a sliding window
+            window = padded_image[i:i + window_size, j:j + window_size]
+            local_entropies.append(calculate_global_entropy(window))
+
+    return np.array(local_entropies)
 
 def calculate_f2(component_images, panorama_image):
-    entropies = [global_entropy(img) for img in component_images]
-    avg_entropy_components = np.mean(entropies)
-
-    panorama_entropy = global_entropy(panorama_image)
-
+    component_entropies = [calculate_global_entropy(img) for img in component_images]
+    avg_entropy_components = np.mean(component_entropies)
+    panorama_entropy = calculate_global_entropy(panorama_image)
     f2 = avg_entropy_components - panorama_entropy
     return f2
 
@@ -27,40 +39,40 @@ f2 = calculate_f2(final_panorama, component_images)
 print(f"f2 (Differential Entropy): {f2}")
 
 # ==============================  f3  ==============================
-
-def local_entropy(image, window_size=9):
-    return entropy(image, disk(window_size // 2))
-
-def calculate_f3(panorama_image):
-    local_entropy_panorama = local_entropy(panorama_image)
-    return np.mean(local_entropy_panorama)
-
-f3 = calculate_f3(final_panorama)
-print(f"f3 (Average Local Entropy): {f3}")
-
-# ==============================  f4  ==============================
-
-def calculate_f4(component_images, panorama_image, window_size=9):
-    local_entropies_components = [local_entropy(img, window_size) for img in component_images]
-    variance_components = [np.var(ent) for ent in local_entropies_components]
-    avg_variance_components = np.mean(variance_components)
-
-    local_entropy_panorama = local_entropy(panorama_image, window_size)
-    variance_panorama = np.var(local_entropy_panorama)
-
-    return variance_panorama - avg_variance_components
-
-f4 = calculate_f4(component_images, final_panorama)
-print(f"f4 (Differential Variance of Local Entropy): {f4}")
-
-# ==============================  f9  ==============================
-
-def calculate_f9(component_images, panorama_image):
-    std_components = [np.std(img) for img in component_images]
-    avg_std_components = np.mean(std_components)
-    std_panorama = np.std(panorama_image)
-    return abs(std_panorama - avg_std_components)
-
-f9 = calculate_f9(component_images, final_panorama)
-print(f"f9 (Absolute Difference of Standard Deviations): {f9}")
-
+#
+# def local_entropy(image, window_size=9):
+#     return entropy(image, disk(window_size // 2))
+#
+# def calculate_f3(panorama_image):
+#     local_entropy_panorama = local_entropy(panorama_image)
+#     return np.mean(local_entropy_panorama)
+#
+# f3 = calculate_f3(final_panorama)
+# print(f"f3 (Average Local Entropy): {f3}")
+#
+# # ==============================  f4  ==============================
+#
+# def calculate_f4(component_images, panorama_image, window_size=9):
+#     local_entropies_components = [local_entropy(img, window_size) for img in component_images]
+#     variance_components = [np.var(ent) for ent in local_entropies_components]
+#     avg_variance_components = np.mean(variance_components)
+#
+#     local_entropy_panorama = local_entropy(panorama_image, window_size)
+#     variance_panorama = np.var(local_entropy_panorama)
+#
+#     return variance_panorama - avg_variance_components
+#
+# f4 = calculate_f4(component_images, final_panorama)
+# print(f"f4 (Differential Variance of Local Entropy): {f4}")
+#
+# # ==============================  f9  ==============================
+#
+# def calculate_f9(component_images, panorama_image):
+#     std_components = [np.std(img) for img in component_images]
+#     avg_std_components = np.mean(std_components)
+#     std_panorama = np.std(panorama_image)
+#     return abs(std_panorama - avg_std_components)
+#
+# f9 = calculate_f9(component_images, final_panorama)
+# print(f"f9 (Absolute Difference of Standard Deviations): {f9}")
+#
