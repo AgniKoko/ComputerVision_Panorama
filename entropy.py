cc9e3f64331beab9ae4470fcba7cae27526674df
AgniKoko <121796084+AgniKoko@users.noreply.github.com>
2024-12-01 16:37:02 +0200

    Added SURF calc

--- entropy.py
+++ entropy.py
@@ -2,41 +2,64 @@ import cv2
 from entropy_calc import calculate_entropy
 
 # ==============================  GES-50  ==============================
-
+print("================= SIFT =================")
+print("")
 component_1 = cv2.imread('results/GES-50/panorama12.jpg')
 component_2 = cv2.imread('results/GES-50/panorama34.jpg')
 final_panorama = cv2.imread('results/GES-50/final_panorama.jpg')
 f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/GES-50/plot.jpg')
 print("")
+print("================= SURF =================")
+print("")
+final_panorama = cv2.imread('results/GES-50/SURF/final_panorama.jpg')
+f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/GES-50/SURF/plot_SURF.jpg')
 
 # ==============================  NISwGSP  ==============================
-
+print("================= SIFT =================")
+print("")
 component_1 = cv2.imread('results/NISwGSP/panorama12.jpg')
 component_2 = cv2.imread('results/NISwGSP/panorama34.jpg')
 final_panorama = cv2.imread('results/NISwGSP/final_panorama.jpg')
 f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/NISwGSP/plot.jpg')
 print("")
+print("================= SURF =================")
+print("")
+final_panorama = cv2.imread('results/NISwGSP/SURF/final_panorama.jpg')
+f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/NISwGSP/SURF/plot_surf.jpg')
 
 # ==============================  flower  ==============================
-
+print("================= SIFT =================")
+print("")
 component_1 = cv2.imread('results/OpenPano/flower/panorama12.jpg')
 component_2 = cv2.imread('results/OpenPano/flower/panorama34.jpg')
 final_panorama = cv2.imread('results/OpenPano/flower/final_panorama.jpg')
 f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/OpenPano/flower/plot.jpg')
 print("")
+print("================= SURF =================")
+print("")
+final_panorama = cv2.imread('results/OpenPano/flower/SURF/final_panorama.jpg')
+f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/OpenPano/flower/SURF/plot_surf.jpg')
 
 # ==============================  augo - scene1  ==============================
-
+print("================= SIFT =================")
+print("")
 component_1 = cv2.imread('results/augo/scene1/panorama12.jpg')
 component_2 = cv2.imread('results/augo/scene1/panorama34.jpg')
 final_panorama = cv2.imread('results/augo/scene1/final_panorama.jpg')
 f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/augo/scene1/plot.jpg')
 print("")
+print("================= SURF =================")
+final_panorama = cv2.imread('results/augo/scene1/SURF/final_panorama.jpg')
+f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/augo/scene1/SURF/plot_surf.jpg')
 
 # ==============================  augo - scene2  ==============================
-
+print("================= SIFT =================")
 component_1 = cv2.imread('results/augo/scene2/panorama12.jpg')
 component_2 = cv2.imread('results/augo/scene2/panorama34.jpg')
 final_panorama = cv2.imread('results/augo/scene2/final_panorama.jpg')
 f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/augo/scene2/plot.jpg')
 print("")
+print("================= SURF =================")
+print("")
+final_panorama = cv2.imread('results/augo/scene2/SURF/final_panorama.jpg')
+f2, f3, f4, f9 = calculate_entropy(component_1, component_2, final_panorama, save_plot_path='results/augo/scene2/SURF/plot_surf.jpg')
